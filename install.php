cc5ebbd2f4415a1d9ff984ca358b2fe79907cc6e
Baitao15 <71545626+Baitao15@users.noreply.github.com>
2021-05-03 11:57:19 +0100

    Add files via upload

---
+++ install.php
@@ -0,0 +1,16 @@
+<?php
+include_once("connection.php");
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS customers;
+CREATE TABLE customers
+(customerid VARCHAR(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(50) NOT NULL,
+password VARCHAR(50) NOT NULL,
+forename INT(20) NOT NULL,
+surname VARCHAR(20) NOT NULL,
+phoneno INT(11) NOT NULL,
+postcode VARCHAR(7) NOT NULL,
+address VARCHAR(30) NOT NULL,
+
+)");
+$stmt->execute();
