f70e51da7fd94a032912c2bee90a273f125f4807
Baitao15 <cbt1529@hotmail.com>
2021-05-26 11:51:52 +0100

    a

--- install.php
+++ install.php
@@ -1,6 +1,8 @@
 <?php
+// linking to connection.php to gain access to the database
 include_once("connection.php");
 
+// creating tables
 $stmt = $conn->prepare("DROP TABLE IF EXISTS customers;
 CREATE TABLE customers
 (customerid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -13,6 +15,15 @@ postcode VARCHAR(7) NOT NULL,
 address VARCHAR(30) NOT NULL,
 cardno INT(16))");
 $stmt->execute();
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS admin;
+CREATE TABLE admin
+(userid INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+forename INT(20) NOT NULL,
+surname VARCHAR(20) NOT NULL,
+password VARCHAR(50) NOT NULL)");
+$stmt->execute();
+
 
 $conn=null;
 
