ab8035354b94c91617555297f1726425d04ded69
Baitao15 <cbt1529@hotmail.com>
2021-05-26 11:40:06 +0100

    a

--- connection.php
+++ connection.php
@@ -1,16 +1,21 @@
 <?php
+// setting variables, which will be used to login to the database
 $servername = "localhost:3306";
 $username = "root";
 $password = "";
 $dbname = "longda";
 
+// attempting to connect to the database
 try {
+    // passing the variabes through to login to the database
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // if the database is accessed successfully, a success message is returned
     echo "Connected successfully"; 
     }
 catch(PDOException $e)
     {
+    // if the connection fails, an error message is returned
     echo "Connection failed: " . $e->getMessage();
     }
 ?>
