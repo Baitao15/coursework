91b15a78f0f00168285e637f558758fd5b94d3c7
Baitao15 <cbt1529@hotmail.com>
2021-06-07 12:50:01 +0100

    a

--- install.php
+++ install.php
@@ -8,23 +8,13 @@ CREATE TABLE customers
 (customerid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(50) NOT NULL,
 password VARCHAR(50) NOT NULL,
-forename INT(20),
+forename VARCHAR(20),
 surname VARCHAR(20),
 phoneno INT(11),
 postcode VARCHAR(7),
 address VARCHAR(30),
 cardno INT(16))");
 $stmt->execute();
-
-$stmt = $conn->prepare("DROP TABLE IF EXISTS admin;
-CREATE TABLE admin
-(userid INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-forename INT(20) NOT NULL,
-surname VARCHAR(20) NOT NULL,
-password VARCHAR(50) NOT NULL)");
-$stmt->execute();
-
-
 
 $conn=null;
 
