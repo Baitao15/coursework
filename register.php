5580a5f82f28c568a0166371bc463a2da3dde4d4
Baitao15 <cbt1529@hotmail.com>
2021-06-07 11:51:57 +0100

    a

--- register.php
+++ register.php
@@ -5,6 +5,7 @@
 </head>
 
 <body>
+<!-- basic text for the page, including a form for users to create an account -->
 Register
     <form action="signup.php" method="POST">
     Email <input type="text" name="email" required><br>
@@ -16,6 +17,22 @@ Register
 </body>
 
 <?php
+// linking to connection.php to gain access to the database
+include_once("connection.php");
+
+// hashing the password
+$hashed_password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+// sending the data to the database
+$stmt = $conn->prepare("INSERT INTO customers(email,password)
+VALUES(:email,:password)");
+
+$stmt->bindParam(':username', $_POST["dusername"]);
+$stmt->bindParam(':password', $hashed_password);
+$stmt->execute();
+
+//redirecting the user to the login page
+header('Location: .php')
 
 ?>
 
