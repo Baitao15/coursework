18cadcfea458a3682731e5020aead54fd8c5d7f9
Baitao15 <cbt1529@hotmail.com>
2021-06-07 11:52:33 +0100

    a

--- register.php
+++ register.php
@@ -9,7 +9,7 @@
 Register
     <form action="signup.php" method="POST">
     Email <input type="text" name="email" required><br>
-    Choose a Password <input type="text" name="password" required><br>
+    Password <input type="text" name="password" required><br>
     <br>
     <input type="submit" value="Register"><br>
     </form>
