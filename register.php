43a92370c30a93ef47069112cb4f9c51d85bee8c
Baitao15 <cbt1529@hotmail.com>
2021-06-07 11:45:10 +0100

    a

---
+++ register.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <title>Sign Up</Title>
+</head>
+
+<body>
+Sign Up
+    <form action="signup.php" method="POST">
+    Email <input type="text" name="email" required><br>
+    Choose a Password <input type="text" name="password" required><br>
+    <br>
+    <input type="submit" value="Create Account"><br>
+    </form>
+    <br>
+</body>
+
+<?php
+
+?>
+
+</html>
