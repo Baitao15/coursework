2ba957d57515b5427b05995bac0bf9096620ab10
Baitao15 <cbt1529@hotmail.com>
2021-06-14 11:33:22 +0100

    a

--- login.php
+++ login.php
@@ -6,4 +6,5 @@ include_once("connection.php");
 
 array_map("htmlspecialchars", $_POST);
 
-$stmt = $conn->prepare("SELECT * FROM ")
+$stmt = $conn->prepare("SELECT * FROM customer WHERE email = :email;");
+$stmt->blindparam(':email', $_POST['email']);
