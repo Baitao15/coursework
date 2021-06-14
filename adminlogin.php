4a97681168368fee90216254978e291d78abb024
Baitao15 <cbt1529@hotmail.com>
2021-06-14 12:50:38 +0100

    a

---
+++ adminlogin.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+
+include_once("connection.php");
+
+array_map("htmlspecialchars", $_POST);
+
+$stmt = $conn->prepare("SELECT * FROM admin WHERE userid = :userid;");
+$stmt->blindparam(':userid', $_POST['userid']);
+$stmt->execute();
+
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    
+    $hashed= $row['password'];
+    $attempt= $_POST['password'];
+    
+    if(password_verify($attempt,$hashed)){
+        $_SESSION['userid']=$row["userid"];
+        if (!isset($_SESSION['backURL'])){
+            $backURL= "/";
+        }else{
+            $backURL=$_SESSION['backURL'];
+        }
+        unset($_SESSION['backURL']);
+        header('Location: ' . $backURL);
+
+        header('Location: adminhomepage.php');
+    
+    }else{
+
+        header('Location: adminloginpage.php');
+    }
+}
---
+++ adminloginpage.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+  <title>Login - Longda Staff</title>
+</head>
+
+<body>
+    Staff Login
+    </div>
+    <br>
+    <form action="adminlogin.php" method="POST">
+        StaffID
+        <br>
+        <input type="text" name="userid" required>
+        <br>
+        <br>
+        Password
+        <br>
+        <input type="password" name="password" required>
+        <br>
+        <br>
+        <input type="submit" value="Login">
+    </form>
+    </div>
+</body>
+
+</html>
