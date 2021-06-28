150dd383854843096be00e744de533e888922e4e
Baitao15 <cbt1529@hotmail.com>
2021-06-28 11:58:27 +0100

    a

--- install.php
+++ install.php
@@ -7,7 +7,7 @@ $stmt = $conn->prepare("DROP TABLE IF EXISTS customer;
 CREATE TABLE customer
 (customerid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(50) NOT NULL,
-password VARCHAR(50) NOT NULL,
+password VARCHAR(255) NOT NULL,
 forename VARCHAR(20),
 surname VARCHAR(20),
 phoneno INT(11),
@@ -21,7 +21,7 @@ CREATE TABLE admin
 (userid INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 forename VARCHAR(20) NOT NULL,
 surname VARCHAR(20) NOT NULL,
-password VARCHAR(50) NOT NULL)");
+password VARCHAR(255) NOT NULL)");
 $stmt->execute();
 
 //inserting defualt data into the admin table
