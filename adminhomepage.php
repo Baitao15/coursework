d5a7bea17e1c1d1554cabfcbcc7f91e4720f0713
Baitao15 <cbt1529@hotmail.com>
2021-06-28 12:15:08 +0100

    a

--- adminhomepage.php
+++ adminhomepage.php
@@ -1,8 +1,25 @@
+<html>
+
 <?php
 
 session_start(); 
-if (!isset($_SESSION['name']))
+if (!isset($_SESSION['userid']))
 {   
     header("Location:adminloginpage.php");
 }
 ?>
+
+<head>
+    <title>Longda Admin - Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- navigation bar -->
+    <p id=navbar>
+        <a href=adminhomepage.php>Home</a>
+        <a href=newadminpage.php>Add Admin Account</a>
+    </p>
+</body>
+
+</html>
