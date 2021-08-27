9d1e1f03e12fee31c8dafd5294bfb3b327b5e8b2
Baitao15 <cbt1529@hotmail.com>
2021-08-27 14:31:35 +0100

    a

--- adminloginpage.php
+++ adminloginpage.php
@@ -2,9 +2,18 @@
 
 <head>
   <title>Login - Longda Staff</title>
+  <link rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
 
 <body>
+  <!-- navigation bar -->
+  <div id=navbar>
+    <a href=adminhomepage.php>Longda</a>
+  </div>
+  <!--  login form  -->
     Staff Login
     </div>
     <br>
