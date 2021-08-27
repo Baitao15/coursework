84bb5c86b01a54ee1cfa1ad57610ed838f285619
Baitao15 <cbt1529@hotmail.com>
2021-08-27 10:22:46 +0100

    a

--- homepage.php
+++ homepage.php
@@ -10,6 +10,11 @@
     <head>
         <title>Longda - Home</title>
         <link rel="stylesheet" href="style.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
 
 <body>
@@ -19,13 +24,13 @@
         <a href=homepage.php>Home</a>
         <a href=groceries.php>Groceries</a>
         <?php 
-        if (!isset($_SESSION['email'])){
-            echo("<a href=registerpage.php>Register</a> <a href=loginpage.php>Login</a>");
-        }
-        if (isset($_SESSION['email'])){  
-            echo("<a href=accountpage.php>Account</a> <a href=logout.php>Logout</a>");
-        }
-    ?>
+            if (!isset($_SESSION['email'])){
+                echo("<a href=registerpage.php>Register</a> <a href=loginpage.php>Login</a>");
+            }
+            if (isset($_SESSION['email'])){  
+                echo("<a href=accountpage.php>Account</a> <a href=logout.php>Logout</a>");
+            }
+        ?>
     </div>
 
     <!-- welcome message
