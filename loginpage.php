af31e549e9c88f3e54cac2373ece5d35b835cc9e
Baitao15 <cbt1529@hotmail.com>
2021-08-27 12:22:28 +0100

    a

--- loginpage.php
+++ loginpage.php
@@ -14,36 +14,38 @@
 
 <head>
   <title>Login - Longda</title>
+  <link rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
 
 <body>
-    <?php
-      if ($loggedin==false)
-      echo('
-        Login
-        </div>
-        <br>
-        <form action="login.php" method="POST">
-            Email
-            <br>
-            <input type="text" name="email" required>
-            <br><br>
-            Password
-            <br>
-            <input type="password" name="password" required>
-            <br><br>
-            <input type="submit" value="Login">
-        </form>
-      ');
+  <!-- return to the homepage -->
+    <p id=navbar>
+        <a href=homepage.php>Home</a>
+    </p>
 
-      else{
-          echo("<p>
-                You are already logged in. <br>
-                Return to the <a href=homepage.php>Home Page</a> <br>
-                <a href=logout.php>Logout</a>
-                </p>");
-        }
-    ?>
+    Login
+    </div>
+    <br>
+    <form action="login.php" method="POST">
+        Email
+        <br>
+        <input type="text" name="email" required>
+        <br><br>
+        Password
+        <br>
+        <input type="password" name="password" required>
+        <br><br>
+        <input type="submit" value="Login">
+    </form>
+        
+        <!-- <p>
+          You are already logged in. <br>
+          Return to the <a href=homepage.php>Home Page</a> <br>
+          <a href=logout.php>Logout</a>
+        </p> -->
     </div>
 </body>
 
