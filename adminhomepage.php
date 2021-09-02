c65fb129c4d4287ad56dc74384dddde1d8651b58
Baitao15 <cbt1529@hotmail.com>
2021-09-02 18:03:03 +0100

    a

--- adminhomepage.php
+++ adminhomepage.php
@@ -31,7 +31,7 @@ $stmt->execute();
         <a href=manageofferpage.php>Offers</a>
         <a href=adminlogout.php>Logout</a>
     </p>
-    <br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br>
     <!-- displaying welcome message -->
     <h3>
     <?php
