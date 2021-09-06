2c2554dc860d6498612154122271b3b2ce0050b7
Baitao15 <cbt1529@hotmail.com>
2021-09-06 11:43:39 +0100

    a

--- changeadminpassword.php
+++ changeadminpassword.php
@@ -17,13 +17,14 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
 // setting variables
     $hashed= $row['password'];
     $cpass= $_POST['cpass'];
-    password_hash($_POST['npass'], PASSWORD_DEFAULT);
+    $npass= password_hash($_POST['npass'], PASSWORD_DEFAULT);
 
 // checking current password match
     if(password_verify($cpass,$hashed)){
         // changing password
         $stmt = $conn->prepare("UPDATE admin(password)
-        SET(:password)");
+        SET(:password)
+        WHERE userid = ");
         $stmt->bindParam(':password', $npass);
         $stmt->execute();
         header('Location: manageaccountpage.php');
