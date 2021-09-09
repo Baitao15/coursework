b7fd8a022bda82d6dcd4f980995819c6d258f195
Baitao15 <cbt1529@hotmail.com>
2021-09-09 11:31:47 +0100

    a

--- addtobasket.php
+++ addtobasket.php
@@ -3,6 +3,13 @@ session_start();
 // creating session variable basket
 // use of 2D array, storing itemid and quantity
 $_SESSION['basket']=array(
-    array()
-)
+
+);
+
+$temp=array(
+
+);
+
+$itemid=$_POST['itemid'];
+
 ?>
