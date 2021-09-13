59585fdf6f03c133ecb8b41b35a0c6009dbec89b
Baitao15 <cbt1529@hotmail.com>
2021-09-13 12:11:14 +0100

    q

--- grocerypage.php
+++ grocerypage.php
@@ -42,14 +42,28 @@
             // getting relevant data from the database
             $stmt = $conn->prepare("SELECT itemid, itemname, itemprice FROM item");
             $stmt->execute();
+            // counter
+            $i=0;
             // displaying each item and its details
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-                echo($row["itemname"]."<br>"."£".$row["itemprice"]."<br>");
+                if ($i=1){
+                    echo('<div class="col-sm-2"></div>');
+                }
+                echo('<div class="col-sm-2">');
+                echo('<div class="panel panel-default text-center">');
+                echo('<div class="panel-heading"><b>'.$row["itemname"]."</b></div><br>"."£".$row["itemprice"]."<br>");
                 echo('<form action="addtobasket.php" method="POST" class="form-inline">');
                 echo('<input type="hidden" name="id" value='.$row["itemid"].">");
                 echo('<input type="number" placeholder="Qty" name="qty" min="1" max="99" value="1" required>');
-                echo('<input type="submit" value="Add" class="btn btn-sm"><br>');
+                echo('<br><input type="submit" value="Add" class="btn btn-sm"><br>');
                 echo("</form><br>");
+                echo('</div>');
+                echo('</div>');
+                if ($i=5){
+                    echo('<div class="col-sm-2"></div>');
+                    $i=0;
+                }
+                $i=($i+1);
             }
         ?>
     </div>
