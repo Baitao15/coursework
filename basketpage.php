f852601dc1bda0b2061defb9f5f61a4ab55b42b6
Baitao15 <cbt1529@hotmail.com>
2021-10-04 12:49:03 +0100

    a

--- basketpage.php
+++ basketpage.php
@@ -63,7 +63,6 @@
         echo("</table>
             <b>Total<br>
             £".$total."</b><br>");
-        
         echo("<form action='checkout.php' method='POST' class='form-inline'>
                 <input type='hidden' name='total' value='$total'>
                 <input type='submit' value='Checkout' class='btn btn-sm'>
