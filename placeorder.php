07f4868fe1a32de85a0cf1a015f83a8b71ee8a0a
Baitao15 <cbt1529@hotmail.com>
2021-11-15 12:38:46 +0000

    a

--- placeorder.php
+++ placeorder.php
@@ -1,17 +1,26 @@
 <?php
 include_once('connection.php');
+
+session_start();
+
 if ($_POST['address1']==""){
     if(!isset($_POST['address'])){
         $_SESSION['message'] = 'Please choose a valid address';
         header('Location: checkoutpage.php');
     }
     else{
-        $addressid=$_POST['address'];
-        $stmt = $conn->prepare("SELECT * FROM address WHERE addressid = $addressid");
-        $stmt->execute();
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            echo($row['address1']);
+        $ordercontents="";
+        for ($i=0; $i<(count($_SESSION['basket'])); $i++){
+            $itemid=($_SESSION['basket'][$i][0]);
+            $qty=($_SESSION['basket'][$i][1]);
+            $ordercontents.= ($itemid.','.$qty.':');
         }
+        $stmt = $conn->prepare("INSERT INTO orderr(addressid, ordercontents)
+        VALUES(:addressid,:ordercontents)");
+
+        $stmt->bindParam(':addressid', $_POST['address']);
+        $stmt->bindParam(':ordercontents', $_SESSION['basket']);
+        $stmt->execute();
     }
 }
 // header('Location: ordersummarypage.php');
