a9b1d39e970bdb03823819dfc95b3fc94922e156
Baitao15 <cbt1529@hotmail.com>
2021-11-22 09:28:28 +0000

    a

--- loginpage.php
+++ loginpage.php
@@ -37,6 +37,7 @@
         // display failed login message if applicable
             if (isset($_SESSION['message'])){
               echo($_SESSION['message']);
+              unset($_SESSION['message']);
             }
         ?>
         <form action="login.php" method="POST">
