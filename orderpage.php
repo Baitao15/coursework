5c488e0021756100b1c3a7bd5394e023bb9db8d3
Baitao15 <cbt1529@hotmail.com>
2021-11-29 12:18:31 +0000

    a

---
+++ orderpage.php
@@ -0,0 +1,78 @@
+<html>
+
+<?php
+// linking to connection.php to gain access to the database
+include_once("connection.php");
+
+// checking authorised user
+session_start(); 
+if (!isset($_SESSION['userid'])){   
+    header("Location:adminloginpage.php");
+}
+?>
+
+<head>
+    <title>View Orders</title>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+
+<body>
+    <!-- navigation bar -->
+    <div id=navbar>
+        <a href=adminhomepage.php>Home</a>
+        <a href=orderpage.php>Orders</a>
+        <a href=insightpage.php>Insights</a>
+        <a href=manageitempage.php>Items</a>
+        <a href=manageofferpage.php>Offers</a>
+        <a href=manageaccountpage.php>Account</a>
+        <a href=adminlogout.php>Logout</a>
+    </div>
+
+    <br><br><br><br>
+
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+            <table class='table'>
+                <tr>
+                    <th>OrderID</th>
+                    <th>Customer</th>
+                    <th>Delivery Address</th>
+                    <th>Order Total</th>
+                </tr>
+                <?php
+                $stmt = $conn->prepare("SELECT * FROM orderr ORDER BY orderid");
+                $stmt->execute();
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                $orderid=$row['orderid'];
+                $customerid=$row['customerid'];
+                $addressid=$row['addressid'];
+
+                $stmt = $conn->prepare("SELECT * FROM customer WHERE customerid=$customerid");
+                $stmt->execute();
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                $forename=$row['forename'];
+                $surname=$row['surname'];
+
+                $stmt = $conn->prepare("SELECT * FROM address WHERE addressid=$addressid");
+                $stmt->execute();
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                $address=$row['address1'];
+
+                
+
+                echo("<tr>");
+                    echo("<td>".$orderid."</td>");
+                    echo("<td>".$forename.' '.$surname."</td>");
+                    echo("<td>".$address."</td>");
+                    echo("<td>".$total."</td>");
+                ?>
+            </table>
+        </div>
+    </div>
+
+</body>
+</html>
