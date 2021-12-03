18ee2a7afa1565505799ab5708a4a5f2489ef5e6
Baitao15 <cbt1529@hotmail.com>
2021-12-03 11:56:23 +0000

    a

---
+++ accountpage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<?php
+    session_start();
+
+    include_once("connection.php");
+
+    $_SESSION['backURL']='accountpage.php';
+?>
+
+<html>
+    <head>
+        <title>Your Account</title>
+        <link rel="stylesheet" href="style.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    </head>
+
+<body>
+
+    <!-- navigation bar -->
+    <div id=navbar>
+        <a href=homepage.php>Home</a>
+        <a href=grocerypage.php>Groceries</a>
+        <a href=basketpage.php>Basket</a>
+        <?php 
+            if (!isset($_SESSION['email'])){
+                echo("<a href=registerpage.php>Register</a> <a href=loginpage.php>Login</a>");
+            }
+            if (isset($_SESSION['email'])){  
+                echo("<a href=accountpage.php>Account</a> <a href=logout.php>Logout</a>");
+            }
+        ?>
+    </div>
+
+    
