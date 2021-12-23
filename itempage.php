967c53671370bbdacf31ba2ef1ae160f8be0db8c
Baitao15 <cbt1529@hotmail.com>
2021-12-23 19:39:14 +0000

    a

--- itempage.php
+++ itempage.php
@@ -65,15 +65,17 @@
                 <br><input type="submit" value="Add" class="btn btn-lg"><br>
             </form><br>
             <hr class="solid2">
-            <div class="col-sm-6">
-                <h3 class='text-center'>Description</h3>
+            <div class=row>
+                <div class="col-sm-6">
+                    <h3 class='text-center'>Description</h3>
+                </div>
+                <div class="col-sm-6">
+                    <h3 class='text-center'>Reviews</h3>
+                </div>
             </div>
-            <div class="col-sm-6">
-                <h3 class='text-center'>Reviews</h3>
-            </div>
-
+            <hr class="solid1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
-
         <div class="col-sm-2">
         </div>
 <!-- if(isset($_GET['id'])){
