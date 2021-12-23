75d3c387904bddb3ed0d63f10aff12797130964b
Baitao15 <cbt1529@hotmail.com>
2021-12-23 19:40:11 +0000

    a

--- itempage.php
+++ itempage.php
@@ -75,8 +75,7 @@
             <hr class="solid1">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
-        <div class="col-sm-2">
-        </div>
+        <div class="col-sm-2"></div>
 <!-- if(isset($_GET['id'])){
     $itemid=$_GET['id'];
     $_SESSION['backURL']='itempage.php?'.$itemid;
