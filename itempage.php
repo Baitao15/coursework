69a0d6db394ec7939a6bd1e6a29f55130ca824d5
Baitao15 <cbt1529@hotmail.com>
2021-12-27 15:53:37 +0000

    a

--- itempage.php
+++ itempage.php
@@ -121,30 +121,8 @@
                 </div>
                 <div class="modal-body">
                     <div class="text-center">
-                        <form action="addreview.php" method="POST" id="writeRevForm">
-                            <label class="label-text" id="rating">Overall Rating</label>
-                            <br>
-                            <div class="stars">
-                                <div class="text-center">
-                                    <input class="star star-5" id="star-5" type="radio" name="star"/>
-                                    <label class="star star-5" for="star-5"></label>
-                                    <input class="star star-4" id="star-4" type="radio" name="star"/>
-                                    <label class="star star-4" for="star-4"></label>
-                                    <input class="star star-3" id="star-3" type="radio" name="star"/>
-                                    <label class="star star-3" for="star-3"></label>
-                                    <input class="star star-2" id="star-2" type="radio" name="star"/>
-                                    <label class="star star-2" for="star-2"></label>
-                                    <input class="star star-1" id="star-1" type="radio" name="star"/>
-                                    <label class="star star-1" for="star-1"></label>
-                                </div>
-                            </div>
-                            <hr class="solid1">
-                            <label class="label-text" for="title">Add a Headline</label><br>
-                            <input type="text" name="title" id="title" style="width: 100%;" placeholder="What's most important to know?" required>
-                            <hr class="solid1">
-                            <label class="label-text" for="text">Add a Written Review</label><br>
-                            <textarea name="text" id="text" style="width: 100%;" rows="5" cols ="1" maxlength="255" placeholder="What did you like or dislike?&#13&#13&#13" required></textarea>
-                            <br><br><input type="submit" value="Submit" class="btn btn-lg"><br>
+                        <form action="addreview.php" method="POST" id="writeRevForm">         
+                            <input type="submit" value="Submit" class="btn btn-lg"><br>
                         </form>
                     </div>
                 </div>
@@ -201,3 +179,26 @@
                         }
                         
                     } -->
+
+                    <!-- <label class="label-text" id="rating">Overall Rating</label>
+                            <br>
+                            <div class="stars">
+                                <div class="text-center">
+                                    <input class="star star-5" id="star-5" type="radio" name="star"/>
+                                    <label class="star star-5" for="star-5"></label>
+                                    <input class="star star-4" id="star-4" type="radio" name="star"/>
+                                    <label class="star star-4" for="star-4"></label>
+                                    <input class="star star-3" id="star-3" type="radio" name="star"/>
+                                    <label class="star star-3" for="star-3"></label>
+                                    <input class="star star-2" id="star-2" type="radio" name="star"/>
+                                    <label class="star star-2" for="star-2"></label>
+                                    <input class="star star-1" id="star-1" type="radio" name="star"/>
+                                    <label class="star star-1" for="star-1"></label>
+                                </div>
+                            </div>
+                            <hr class="solid1">
+                            <label class="label-text" for="title">Add a Headline</label><br>
+                            <input type="text" name="title" id="title" style="width: 100%;" placeholder="What's most important to know?" required>
+                            <hr class="solid1">
+                            <label class="label-text" for="text">Add a Written Review</label><br>
+                            <textarea name="text" id="text" style="width: 100%;" rows="5" cols ="1" maxlength="255" placeholder="What did you like or dislike?&#13&#13&#13" required></textarea> -->
