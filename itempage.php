ee7ded092e1f1b6c3c61cc47e06837cac9c110a7
Baitao15 <cbt1529@hotmail.com>
2021-12-27 16:04:57 +0000

    a

--- itempage.php
+++ itempage.php
@@ -129,26 +129,26 @@
         </div>
     </div>
 <script>
-    // Get the modal
+    // get modal
     var modal = document.getElementById("writeRevModal");
 
-    // Get the button that opens the modal
+    // get the button that opens the modal
     var btn = document.getElementById("writeRevBtn");
 
-    // Get the <span> element that closes the modal
+    // get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks the button, open the modal 
+    // if user clicks button, open modal 
     btn.onclick = function() {
         modal.style.display = "block";
     }
 
-    // When the user clicks on <span> (x), close the modal
+    // if user clicks on <span> (i.e. "x"), close  modal
     span.onclick = function() {
         modal.style.display = "none";
     }
 
-    // When the user clicks anywhere outside of the modal, close it
+    // if user clicks anywhere outside of the modal, close modal
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
