3d9251ff5e854815ab6728d8063b309bef3d852b
Baitao15 <cbt1529@hotmail.com>
2021-12-27 17:05:37 +0000

    a

---
+++ addreview.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include_once("connection.php");
+
+$stmt = $conn->prepare("INSERT INTO review(customerid, itemid, reviewtitle, reviewtext, stars)
+VALUES(:customerid, :itemid ,:reviewtitle, :reviewtext, :stars)");
+
+$customerid=$_SESSION['id'];
+$itemid=$_POST['itemid'];
+$title=$_POST['title'];
+$text=$_POST['text'];
+$star=$_POST['star'];
+
+$stmt->bindParam(':customerid', $customerid);
+$stmt->bindParam(':itemid', $itemid);
+$stmt->bindParam(':reviewtitle', $title);
+$stmt->bindParam(':reviewtext', $text);
+$stmt->bindParam(':stars', $stars);
+$stmt->execute();
+?>
