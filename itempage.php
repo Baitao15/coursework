85480877a486474616adacfc8dff6ad6881f0baa
Baitao15 <cbt1529@hotmail.com>
2021-12-27 13:36:03 +0000

    a

--- itempage.php
+++ itempage.php
@@ -27,33 +27,7 @@
         </title>
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <script>
-            // Get the modal
-            var modal = document.getElementById("writeRevModal");
-
-            // Get the button that opens the modal
-            var btn = document.getElementById("writeRevBtn");
-
-            // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName("close")[0];
-
-            // When the user clicks the button, open the modal 
-            btn.onclick = function() {
-            modal.style.display = "block";
-            }
-
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
-            modal.style.display = "none";
-            }
-
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-            }
-        </script>
+        
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
@@ -148,6 +122,35 @@
             </div>
         </div>
         <div class="col-sm-2"></div>
+<script>
+    // Get the modal
+    var modal = document.getElementById("writeRevModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("writeRevBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>
+</body>
+
 <!-- if(isset($_GET['id'])){
     $itemid=$_GET['id'];
     $_SESSION['backURL']='itempage.php?'.$itemid;
