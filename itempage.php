eb8bcf3ecf5034585bc4d0eb789cbacb1890209b
Baitao15 <cbt1529@hotmail.com>
2021-12-27 16:45:17 +0000

    a

--- itempage.php
+++ itempage.php
@@ -126,15 +126,15 @@
                         <br>
                         <div class="stars">
                             <div class="text-center">
-                                <input class="star star-5" id="star-5" type="radio" name="star"/>
+                                <input class="star star-5" id="star-5" type="radio" name="star" required>
                                 <label class="star star-5" for="star-5"></label>
-                                <input class="star star-4" id="star-4" type="radio" name="star"/>
+                                <input class="star star-4" id="star-4" type="radio" name="star">
                                 <label class="star star-4" for="star-4"></label>
-                                <input class="star star-3" id="star-3" type="radio" name="star"/>
+                                <input class="star star-3" id="star-3" type="radio" name="star">
                                 <label class="star star-3" for="star-3"></label>
-                                <input class="star star-2" id="star-2" type="radio" name="star"/>
+                                <input class="star star-2" id="star-2" type="radio" name="star">
                                 <label class="star star-2" for="star-2"></label>
-                                <input class="star star-1" id="star-1" type="radio" name="star"/>
+                                <input class="star star-1" id="star-1" type="radio" name="star">
                                 <label class="star star-1" for="star-1"></label>
                             </div>
                         </div>
@@ -146,6 +146,7 @@
                         <label class="label-text" for="text">Add a Written Review</label><br>
                         <textarea name="text" id="text" style="width: 100%;" rows="5" cols ="1" maxlength="255"
                         placeholder="What did you like or dislike? (maximum 255 characters)" required></textarea>
+                        <input type="hidden" name="itemid" value=<?php echo($itemid)?>>
                         <input type="submit" value="Submit" class="btn btn-lg"><br>
                     </form>
                 </div>
