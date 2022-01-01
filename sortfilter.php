37284e609580b7a50956dc72dbe7b884a3a9fbd8
Baitao15 <cbt1529@hotmail.com>
2022-01-01 14:14:22 +0000

    a

---
+++ sortfilter.php
@@ -0,0 +1,18 @@
+<?php
+if($_POST['type']=='sort'){
+    $order=$_POST['order'];
+    $sort=$_POST['sort'];
+    if($_POST['filter']=='none'){
+        header('Location: grocerypage.php?sort='.$sort.'&order='.$order);
+    }
+    else{
+        $filter=$_POST['filter'];
+        header('Location: grocerypage.php?sort='.$sort.'&order='.$order.'&cat='.$filter);
+    }
+}
+
+// if($_POST['type']=='filter'){
+//     $filter=$_POST['']
+    
+// }
+?>
