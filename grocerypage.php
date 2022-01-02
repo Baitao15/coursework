75e7b57fc251b4126b77b01d0e09575d4abe7fb1
Baitao15 <cbt1529@hotmail.com>
2022-01-02 15:14:32 +0000

    a

--- grocerypage.php
+++ grocerypage.php
@@ -70,68 +70,68 @@
             }
             $stmt->execute();
         ?>
-        <!-- sort and filter buttons-->
-        <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="dropdown">
-                <button id="sortBtn" class="btn dropbtn" onclick="sortDrop()">
-                    Sort
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-left" viewBox="0 0 16 16">
-                        <path d="M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
-                    </svg>
-                </button>
-                <div id="sortDropdown" class="dropdown-content">
-                    <form action="sortfilter.php" method="POST" id="sortForm" style="padding: 10%;">
-                        <div class="text-center">
-                            <input id="order-asc" type="radio" value="ASC" name="order" style="display: none;" required>
-                            <label id="order-asc" for="order-asc">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-sort-down-alt" viewBox="0 0 16 16">
-                                    <path d="M3.5 3.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 12.293V3.5zm4 .5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1h-1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1h-3zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1h-5zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5z"/>
-                                </svg>
-                            </label>
-                            <input id="order-des" type="radio" value="DESC" name="order" style="display: none;">
-                            <label id="order-des" for="order-des">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-sort-up" viewBox="0 0 16 16">
-                                    <path d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707V12.5zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"/>
-                                </svg>
-                            </label>
-                        </div>
-                        <hr class="solid1">
-                        <input id="price" type="radio" value="itemprice" name="sort" required>
-                        <label for="price">Price</label><br>
-                        <input id="name" type="radio" value="itemname" name="sort">
-                        <label for="name">Name</label>
-                        <input type="hidden" name="type" value='sort'>
-                        <input type="hidden" name="filter" value=<?php if(isset($category)){echo($category);} else{echo('none');}?>>
-                        <br><div class="text-center"><input type="submit" value="Apply" class="btn btn-lg"></div>
-                    </form>
-                </div>
-            </div>
-            <div class="dropdown">
-                <button id="filterBtn" class="btn dropbtn" onclick="filterDrop()">
-                    Filter
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
-                        <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"/>
-                    </svg>
-                </button>
-                <div id="filterDropdown" class="dropdown-content">
-                    <form action="sortfilter.php" method="POST" id="filterForm" style="padding: 10%;">
-                        <label for="category">Category</label>
-                        <div id="category">
-                            <input id="produce" type="checkbox" value="produce" name="category" required>
-                            <label for="produce">Fruit & Veg</label><br>
-                            <input id="bakery" type="checkbox" value="bakery" name="category">
-                            <label for="bakery">Bakery</label><br>
-                            <input id="fresh" type="checkbox" value="fresh" name="category">
-                            <label for="fresh">Fresh</label>
-                        </div>
-                        <input type="hidden" name="type" value='filter'>
-                        <input type="hidden" name="sort" value=<?php if(isset($sort)){echo($sort);}?>>
-                        <br><div class="text-center"><input type="submit" value="Apply" class="btn btn-lg"></div>
-                    </form>
-                </div>
+    <!-- sort and filter buttons-->
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="dropdown">
+            <button id="sortBtn" class="btn dropbtn" onclick="sortDrop()">
+                Sort
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-left" viewBox="0 0 16 16">
+                    <path d="M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+            </button>
+            <div id="sortDropdown" class="dropdown-content">
+                <form action="sortfilter.php" method="POST" id="sortForm" style="padding: 10%;">
+                    <div class="text-center">
+                        <input id="order-asc" type="radio" value="ASC" name="order" style="display: none;" required>
+                        <label id="order-asc" for="order-asc">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-sort-down-alt" viewBox="0 0 16 16">
+                                <path d="M3.5 3.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 12.293V3.5zm4 .5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1h-1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1h-3zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1h-5zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5z"/>
+                            </svg>
+                        </label>
+                        <input id="order-des" type="radio" value="DESC" name="order" style="display: none;">
+                        <label id="order-des" for="order-des">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-sort-up" viewBox="0 0 16 16">
+                                <path d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707V12.5zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"/>
+                            </svg>
+                        </label>
+                    </div>
+                    <hr class="solid1">
+                    <input id="price" type="radio" value="itemprice" name="sort" required>
+                    <label for="price">Price</label><br>
+                    <input id="name" type="radio" value="itemname" name="sort">
+                    <label for="name">Name</label>
+                    <input type="hidden" name="type" value='sort'>
+                    <input type="hidden" name="filter" value=<?php if(isset($category)){echo($category);} else{echo('none');}?>>
+                    <br><div class="text-center"><input type="submit" value="Apply" class="btn btn-lg"></div>
+                </form>
             </div>
         </div>
+        <div class="dropdown">
+            <button id="filterBtn" class="btn dropbtn" onclick="filterDrop()">
+                Filter
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
+                    <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"/>
+                </svg>
+            </button>
+            <div id="filterDropdown" class="dropdown-content">
+                <form action="sortfilter.php" method="POST" id="filterForm" style="padding: 10%;">
+                    <label for="category">Category</label>
+                    <div id="category">
+                        <input id="produce" type="checkbox" value="produce" name="category" required>
+                        <label for="produce">Fruit & Veg</label><br>
+                        <input id="bakery" type="checkbox" value="bakery" name="category">
+                        <label for="bakery">Bakery</label><br>
+                        <input id="fresh" type="checkbox" value="fresh" name="category">
+                        <label for="fresh">Fresh</label>
+                    </div>
+                    <input type="hidden" name="type" value='filter'>
+                    <input type="hidden" name="sort" value=<?php if(isset($sort)){echo($sort);}?>>
+                    <br><div class="text-center"><input type="submit" value="Apply" class="btn btn-lg"></div>
+                </form>
+            </div>
+        </div>
+    </div>
         <?php
             // counter
             $i=0;
