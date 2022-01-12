f12fd8f67de3ed66c29c54710dd2c2f414958f80
Baitao15 <cbt1529@hotmail.com>
2022-01-12 09:06:46 +0000

    a

--- login.php
+++ login.php
@@ -10,26 +10,32 @@ $stmt = $conn->prepare("SELECT * FROM customer WHERE email = :email;");
 $stmt->bindparam(':email', $_POST['email']);
 $stmt->execute();
 
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    
-    $hashed= $row['password'];
-    $attempt= $_POST['password'];
-    
-    if(password_verify($attempt,$hashed)){
-        $_SESSION['id']=$row["customerid"];
-        $_SESSION['email']=$row["email"];
-        // if no backURL is set, then default redirect to homepage
-        if (!isset($_SESSION['backURL'])){
-            $backURL= "homepage.php";
-        }else{
-            $backURL=$_SESSION['backURL'];
+if($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        
+        $hashed= $row['password'];
+        $attempt= $_POST['password'];
+        
+        if(password_verify($attempt,$hashed)){
+            $_SESSION['id']=$row["customerid"];
+            $_SESSION['email']=$row["email"];
+            // if no backURL is set, then default redirect to homepage
+            if (!isset($_SESSION['backURL'])){
+                $backURL= "homepage.php";
+            }else{
+                $backURL=$_SESSION['backURL'];
+            }
+            unset($_SESSION['backURL']);
+            header('Location: ' . $backURL);
         }
-        unset($_SESSION['backURL']);
-        header('Location: ' . $backURL);
-    }
-    else{
-        $_SESSION['message']='Incorrect email or password, please try again';
-        header('Location: loginpage.php');
+        else{
+            $_SESSION['message']='Incorrect email or password, please try again';
+            header('Location: loginpage.php');
+        }
     }
 }
-
+else{
+    $_SESSION['message']='Incorrect email or password, please try again';
+    header('Location: loginpage.php');
+}
