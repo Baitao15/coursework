4e2ee8d8a5346bdb7627ceb250bb737b4512fdb4
Baitao15 <cbt1529@hotmail.com>
2022-01-15 09:12:35 +0000

    a

--- install.php
+++ install.php
@@ -8,9 +8,9 @@ CREATE TABLE customer
 (customerid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(50) NOT NULL,
 password VARCHAR(255) NOT NULL,
-forename VARCHAR(20),
-surname VARCHAR(20),
-phoneno INT(11))");
+forename VARCHAR(20) NOT NULL,
+surname VARCHAR(20) NOT NULL,
+phoneno VARCHAR(12) NOT NULL)");
 $stmt->execute();
 
 // inserting defualt data into the customer table
