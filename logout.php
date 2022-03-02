f0283b0ac21cd00945041d7ba5ee27320fe7e25f
Baitao15 <cbt1529@hotmail.com>
2022-03-02 14:28:08 +0000

    a

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
+// gain access to session variables
 session_start();
 
+// unset all existing session variables
 if(isset($_SESSION['id']))
 {
     unset($_SESSION['id']);
@@ -16,6 +18,7 @@ if(isset($_SESSION['basket']))
     unset($_SESSION['basket']);
 }
 
+// redirect user to home page
 header("Location: homepage.php");
 ?>
 
