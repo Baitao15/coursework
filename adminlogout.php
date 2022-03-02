9e52811dcc1e3dbac409d583dafe42a30b59a51a
Baitao15 <cbt1529@hotmail.com>
2022-03-02 14:47:21 +0000

    a

--- adminlogout.php
+++ adminlogout.php
@@ -1,10 +1,14 @@
 <?php
+// start session to access session variables
 session_start();
 
+// if userid session variable is set
 if(isset($_SESSION['userid']))
 {
+    // unset it
     unset($_SESSION['userid']);
 }
 
-header("Location: adminhomepage.php");
+// redirect user to login page
+header("Location: adminloginpage.php");
 ?>
