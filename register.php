4619b900fadccd0a2719e633d092b9bb5a11b325
Baitao15 <cbt1529@hotmail.com>
2022-03-02 12:54:26 +0000

    a

--- register.php
+++ register.php
@@ -1,17 +1,17 @@
 <?php
+// start session to access email session variable
+session_start();
 // linking to connection.php to gain access to the database
 include_once("connection.php");
-
-session_start();
 
 // checking the email is not already associated with an account
 $email=$_POST["email"];
 $stmt = $conn->prepare("SELECT email FROM customer WHERE email = :email;");
 $stmt->bindparam(':email', $email);
 $stmt->execute();
-
+// if the email is found in the database
 if($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    // if the email is found in the database, redirect user to register page with error message
+    // redirect user to register page with error message
     $_SESSION['message']='Email is already registered with an account. <a href=loginpage.php>Login</a>';
     header('Location: registerpage.php');
 }
