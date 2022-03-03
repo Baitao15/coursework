c4a86ac3cb6351c69ee9bb1e484c4244cbb2ce26
Baitao15 <cbt1529@hotmail.com>
2022-03-03 09:45:45 +0000

    a

--- basketpage.php
+++ basketpage.php
@@ -16,8 +16,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    </head>
-
+    </head>a
 <body>
     <!-- navigation bar -->
     <div id=navbar>
