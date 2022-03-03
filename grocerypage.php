5cd6ca7a6df2ec583b157eda0524f90de2247f6d
Baitao15 <cbt1529@hotmail.com>
2022-03-03 08:49:41 +0000

    a

--- grocerypage.php
+++ grocerypage.php
@@ -125,6 +125,7 @@
                         <input id="name" type="radio" value="itemname" name="sort">
                         <label for="name">Name</label>
                         <input type="hidden" name="type" value='sort'>
+                        <!-- hidden input to allow for sorts and filters to be applied at the same time -->
                         <input type="hidden" name="filter" value=<?php if(isset($cat)){echo($cat);} else{echo('none');}?>>
                         <br><div class="text-center"><input type="submit" value="Apply" class="btn btn-lg"></div>
                     </form>
@@ -133,8 +134,11 @@
             <div class="dropdown">
                 <button id="filterBtn" class="btn dropbtn" onclick="filterDrop()">
                     Filter
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
-                        <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
+                        <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 
+                        1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 
+                        4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"/>
                     </svg>
                 </button>
                 <div id="filterDropdown" class="dropdown-content" style="width: 250%;">
