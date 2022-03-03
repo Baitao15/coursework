e0a497dfa5c3ffdb2884e4ef49da4160cbbd5ffb
Baitao15 <cbt1529@hotmail.com>
2022-03-03 08:50:40 +0000

    a

--- grocerypage.php
+++ grocerypage.php
@@ -134,6 +134,7 @@
             <div class="dropdown">
                 <button id="filterBtn" class="btn dropbtn" onclick="filterDrop()">
                     Filter
+                    <!-- link to bootstrap filter funnel vector icon -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                     fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
                         <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 
